2ae981d9647d48732ce47e81f6d07d648d06ab0b
Apple27022002 <100838894+Apple27022002@users.noreply.github.com>
2022-04-06 10:13:15 +0700

    m

---
+++ src/arr.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class arr {
+    public static void main(String[] args) {
+        java.util.Scanner input = new Scanner(System.in);
+        System.out.println("Enter " + .length + " rows and " +
+                matrix[0].length + " columns: ");
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                matrix[row][column] = input.nextInt();
+            }
+
+    }
+}
